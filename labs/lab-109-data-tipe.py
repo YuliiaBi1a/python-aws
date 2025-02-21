023615627146f68592768fa16696ff752e3dc249
EC2 Default User <ec2-user@ip-10-16-10-35.us-west-2.compute.internal>
2025-02-21 15:23:32 +0000

    add labs 108-115

---
+++ labs/lab-109-data-tipe.py
@@ -0,0 +1,26 @@
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1  
+print(myValue)
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myFloat=3.14
+print(myFloat)
+print(type(myFloat))
+print(str(myFloat) + " is of the data type " + str(type(myFloat)))
+
+myComplex=5j
+print(myComplex)
+print(type(myComplex))
+print(str(myComplex) + " is of the data type " + str(type(myComplex)))
+
+myBool1=True
+print(myBool1)
+print(type(myBool1))
+print(str(myBool1) + " is of the data type " + str(type(myBool1)))
+myBool0=False
+print(myBool0)
+print(type(myBool0))
+print(str(myBool0) + " is of the data type " + str(type(myBool0)))
---
+++ labs/lab-111-colections.py
@@ -0,0 +1,32 @@
+#LIST
+myFruits = ["apple","banana","cherry"]
+print(myFruits)
+print(type(myFruits))
+print(myFruits[0])
+print(myFruits[1])
+print(myFruits[2])
+
+myFruits[2] = "orange"
+print(myFruits)
+
+#DUPLE - cannot modify
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+#DICTIONARY key:value
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple",
+  "age" : 3
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+print(myFavoriteFruitDictionary["age"])
